0f1a21c7a941f9ae20062a62cc550c21e9c9db1c
Kuseh Simon Wewoliamo <kusehsw@gmail.com>
2023-01-05 22:00:46 +0000

    styled the navbar

---
+++ src/Avatar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { FaUserAlt } from "react-icons/fa";
+const Avatar = () => {
+  return (
+    <div className="avatar">
+      <FaUserAlt />
+    </div>
+  );
+};
+
+export default Avatar;
--- src/components/navbar/Nav.jsx
+++ src/components/navbar/Nav.jsx
@@ -1,7 +1,35 @@
 import React from "react";
+import Avatar from "../../Avatar";
+import "./nav.css";
+// Icons
+import { FiMenu } from "react-icons/fi";
+import { IoLogoYoutube } from "react-icons/io";
+import { BsBellFill } from "react-icons/bs";
+import { RiVideoAddFill } from "react-icons/ri";
+import { FaMicrophone } from "react-icons/fa";
 
 const Nav = () => {
-  return <nav className="container nav__container"></nav>;
+  return (
+    <nav className="container nav__container">
+      <div className="logo__container flex__container">
+        <FiMenu className="icon" />
+        <IoLogoYoutube className="icon" />
+      </div>
+
+      <div className="form__container flex__container">
+        <form action="#">
+          <input type="text" placeholder="Search..." />
+        </form>
+        <FaMicrophone className="icon" />
+      </div>
+
+      <div className="user__details_container flex__container">
+        <RiVideoAddFill className="icon" />
+        <BsBellFill className="icon" />
+        <Avatar />
+      </div>
+    </nav>
+  );
 };
 
 export default Nav;
