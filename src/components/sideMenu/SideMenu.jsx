d7a115c9412f89f49cebd52fdb51bea94e81cdbd
Kuseh Simon Wewoliamo <kusehsw@gmail.com>
2023-01-06 22:41:02 +0000

    added aside menu items

--- src/components/sideMenu/SideMenu.jsx
+++ src/components/sideMenu/SideMenu.jsx
@@ -1,8 +1,38 @@
 import React from "react";
 import "./sidemenu.css";
 
+import { AiFillHome } from "react-icons/ai";
+import { MdSubscriptions } from "react-icons/md";
+import { RiMovie2Fill } from "react-icons/ri";
+
 const SideMenu = () => {
-  return <aside>SideMenu</aside>;
+  return (
+    <aside>
+      <div className="aside__items ">
+        <div className="aside_item">
+          <AiFillHome /> <span>Home</span>
+        </div>
+        <div className="aside_item">
+          <RiMovie2Fill /> <span>Inshots</span>
+        </div>
+        <div className="aside_item">
+          <MdSubscriptions /> <span>Subscriptions</span>
+        </div>
+      </div>
+      {/* 
+      <div className="aside__items ">
+        <div className="aside_item">
+          <AiFillHome /> <span>Home</span>
+        </div>
+        <div className="aside_item">
+          <MdSubscriptions /> <span>Home</span>
+        </div>
+        <div className="aside_item">
+          <MdSubscriptions /> <span>Home</span>
+        </div>
+      </div> */}
+    </aside>
+  );
 };
 
 export default SideMenu;
