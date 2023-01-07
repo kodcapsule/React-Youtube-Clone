781ba162b2ec60484b9a51f20529466549d82011
Kuseh Simon Wewoliamo <kusehsw@gmail.com>
2023-01-07 22:56:38 +0000

    added aside items and styled them

--- src/components/mainContent/MaintContent.jsx
+++ src/components/mainContent/MaintContent.jsx
@@ -2,7 +2,21 @@ import React from "react";
 import "./maincontent.css";
 
 const MaintContent = () => {
-  return <section>MaintContent</section>;
+  return (
+    <section>
+      MaintContent Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui
+      numquam vero temporibus voluptates est nam repellat asperiores corporis
+      dolorem amet quasi unde hic magni reprehenderit, officia aut totam tempora
+      tempore omnis odit laudantium atque. Quibusdam ab nam expedita. Eum quis
+      molestiae commodi? Perspiciatis consequuntur quidem dolore laudantium
+      debitis aspernatur reiciendis qui aperiam repellendus est, cum dolorum
+      sint labore deleniti, molestiae enim, repudiandae molestias aut. Fugiat
+      voluptas laudantium nulla recusandae incidunt sit numquam in voluptatibus
+      ipsa illo delectus facere harum veritatis quaerat, aspernatur temporibus
+      aut provident, ea hic iure exercitationem. Ex quibusdam tenetur architecto
+      et quaerat. Id quia earum nesciunt doloremque.
+    </section>
+  );
 };
 
 export default MaintContent;
--- src/components/sideMenu/SideMenu.jsx
+++ src/components/sideMenu/SideMenu.jsx
@@ -4,12 +4,17 @@ import "./sidemenu.css";
 import { AiFillHome } from "react-icons/ai";
 import { MdSubscriptions } from "react-icons/md";
 import { RiMovie2Fill } from "react-icons/ri";
+import { MdVideoLibrary } from "react-icons/md";
+import { GrHistory } from "react-icons/gr";
+import { RiVideoFill } from "react-icons/ri";
+import { BsFillClockFill } from "react-icons/bs";
+import { MdThumbUp } from "react-icons/md";
 
 const SideMenu = () => {
   return (
     <aside>
       <div className="aside__items ">
-        <div className="aside_item">
+        <div className="aside_item active ">
           <AiFillHome /> <span>Home</span>
         </div>
         <div className="aside_item">
@@ -17,6 +22,62 @@ const SideMenu = () => {
         </div>
         <div className="aside_item">
           <MdSubscriptions /> <span>Subscriptions</span>
+        </div>
+      </div>
+      {/* ============================================================= */}
+
+      <div className="aside__items ">
+        <div className="aside_item active ">
+          <MdVideoLibrary /> <span>library</span>
+        </div>
+        <div className="aside_item">
+          <GrHistory /> <span>History</span>
+        </div>
+        <div className="aside_item">
+          <RiVideoFill /> <span>Your Videos</span>
+        </div>
+        <div className="aside_item">
+          <BsFillClockFill /> <span>Watch Later </span>
+        </div>
+        <div className="aside_item">
+          <MdThumbUp /> <span>Likes </span>
+        </div>
+      </div>
+      {/* ============================================================= */}
+
+      <div className="aside__items ">
+        <div className="aside_item active ">
+          <MdVideoLibrary /> <span>library</span>
+        </div>
+        <div className="aside_item">
+          <GrHistory /> <span>History</span>
+        </div>
+        <div className="aside_item">
+          <RiVideoFill /> <span>Your Videos</span>
+        </div>
+        <div className="aside_item">
+          <BsFillClockFill /> <span>Watch Later </span>
+        </div>
+        <div className="aside_item">
+          <MdThumbUp /> <span>Likes </span>
+        </div>
+      </div>
+
+      <div className="aside__items ">
+        <div className="aside_item active ">
+          <MdVideoLibrary /> <span>library</span>
+        </div>
+        <div className="aside_item">
+          <GrHistory /> <span>History</span>
+        </div>
+        <div className="aside_item">
+          <RiVideoFill /> <span>Your Videos</span>
+        </div>
+        <div className="aside_item">
+          <BsFillClockFill /> <span>Watch Later </span>
+        </div>
+        <div className="aside_item">
+          <MdThumbUp /> <span>Likes </span>
         </div>
       </div>
     </aside>
